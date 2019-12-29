bef05c2b225e76fa5c3be9c0ba6f974d9bf23898
Michael Kriese <michael.kriese@visualon.de>
2019-12-29 12:06:51 +0100

    feat: initial android project

---
+++ app/src/main/java/info/kriese/birthdayreminder/MainActivity.kt
@@ -0,0 +1,12 @@
+package info.kriese.birthdayreminder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
